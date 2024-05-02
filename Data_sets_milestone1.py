228ec31ea7880ec746683839eebaaddc0db3a3d0
mandyche <157666307+mandyche@users.noreply.github.com>
2024-05-01 22:42:43 -0700

    Delete Data_sets_milestone1.py

--- Data_sets_milestone1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title('Datasets (Milestone1)')
-st.write('Dataset1: Weatherstack API; This Weatherstack API was built to deliver accurate weather data for any application and use case, from real-time and historical weather information all the way to 14-day weather forecasts.')
-st.write('Dataset2: Yahoo Finance; This website contains accurate and timely data on basic stock information like price, volume, market cap, earnings dividends, basic ratios, analyst ratings and news.')
-st.write('Dataset3: BBC News; This website lists news/articles related to financial planning, stock markets, and personal finance rankings.')
